a96bd98d4a11de0426679df998939813b1c0d72f
jlcanovas <me@jlcanovas.es>
2013-11-14 10:20:10 +0100

    Added Eclipse plugin modified not to use xtext

---
+++ fr.inria.atlanmod.json.discoverer.ui/src/fr/inria/atlanmod/ui/discoverer/JsonModiscoDiscoverer.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2013
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Javier Canovas (javier.canovas@inria.fr) 
+ *******************************************************************************/
+
+package fr.inria.atlanmod.ui.discoverer;
+
+/**
+ * Main superclass to factor common behaviour 
+ * 
+ * @author Javier Canovas (javier.canovas@inria.fr)
+ *
+ */
+public abstract class JsonModiscoDiscoverer {
+	public static String EXTENSION = "json";
+	public static String ECORE_EXTENSION = "ecore";
+}
