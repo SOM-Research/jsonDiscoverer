a0f5a582b48253dd979302ee52c5d22914c7dcb9
jlcanovas <me@jlcanovas.es>
2014-02-10 16:54:22 +0100

    Added support for composition discovery

--- js/script.js
+++ js/script.js
@@ -198,7 +198,7 @@ jsonDiscovererModule.controller("AdvancedDiscovererCtrl", ["$scope", "$rootScope
                 resolve: {
                     jsonName : function() {
                         return jsonName;
-                    }},
+                    }}
             });
 
             modalInstance.result.then(
@@ -246,10 +246,139 @@ var JsonProvisionModalInstanceCtrlVar = function($scope, $modalInstance, $log, j
     };
 }
 
-jsonDiscovererModule.controller("CompositionCtrl", ["$scope", "$window", "$location",
-    function($scope, $window, $location) {
+var JsonProvisionModalWithInputInstanceCtrlVar = function($scope, $modalInstance, $log, jsonName) {
+    $scope.json = { name: jsonName, input: '', output: '' };
+
+    $scope.ok = function() {
+        $modalInstance.close({ name : jsonName, input: $scope.json.input, output: $scope.json.output });
+    };
+
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+}
+
+jsonDiscovererModule.controller("CompositionCtrl", ["$scope", "$window", "$location", "$modal", "$http",
+    function($scope, $window, $location, $modal, $http) {
+        $scope.defs = {} ;
+        $scope.compositionPosible = false;
+        $scope.name = "";
+        $scope.graph = "";
+
+        $scope.sigmaGraph = new sigma('sigma-container');
+        $scope.layoutButton = "Start Layout";
+        $scope.layoutRunning = false;
+
+        $scope.alertsGeneral = [ ];
+
         $scope.$on('$viewContentLoaded', function(event) {
             $window.ga('send', 'pageview', {'page': $location.path()});
         });
+
+
+        $scope.newSource = function() {
+            $scope.defs[$scope.name] = { name : $scope.name, jsonDefs : [] };
+            $scope.name = "";
+        };
+
+        $scope.provideJson = function (jsonName) {
+            var modalInstance = $modal.open({
+                templateUrl: 'jsonProvisionModalWithInput.html',
+                controller: JsonProvisionModalWithInputInstanceCtrlVar,
+                resolve: {
+                    jsonName : function() {
+                        return jsonName;
+                    }}
+            });
+
+            modalInstance.result.then(
+                function(data) {
+                    $scope.defs[data.name]["jsonDefs"].push({ "input" : data.input, "output" : data.output});
+                    $scope.updateCompositionPosible();
+                },
+                function(data) {
+                    //$log.info('Modal dismissed at: ' + new Date());
+                });
+        };
+
+        $scope.closeGeneralAlert = function(index) {
+            $scope.alertsGeneral.splice(index, 1);
+        };
+
+        $scope.discoverComposition = function() {
+            $scope.metamodel = "images/loading.gif";
+
+            var dataToSend = $.param({ sources : $scope.defs });
+
+            $http({
+                method : 'POST',
+                //url : "http://apps.jlcanovas.es/jsonDiscoverer/compose",
+                url : "http://localhost:8080/fr.inria.atlanmod.json.web/discoverComposition",
+                data : dataToSend,
+                headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function(data) {
+                var dataDom = new DOMParser().parseFromString(data, "application/xml");
+                var mygexf = GexfParser.parse(dataDom);
+                console.log("nodes " + mygexf.nodes.length);
+
+                $scope.sigmaGraph.graph.clear();
+                var i, l, arr, obj, node;
+
+                // Adapt the graph:
+                arr = mygexf.nodes;
+                for (i = 0, l = arr.length; i < l; i++) {
+                    obj = arr[i];
+                    $scope.sigmaGraph.graph.addNode({
+                        id: obj.id,
+                        label : obj.label,
+                        x : Math.random(),
+                        y : Math.random(),
+                        size : 1,
+                        color : obj.viz.color
+                    })
+                }
+
+                arr = mygexf.edges;
+                for (i = 0, l = arr.length; i < l; i++) {
+                    obj = arr[i];
+                    $scope.sigmaGraph.graph.addEdge({
+                        id: obj.id,
+                        source : obj.source,
+                        target : obj.target
+                    })
+                }
+                $scope.sigmaGraph.refresh();
+
+            }).error(function(data, status, headers, config) {
+                $scope.metamodel = "";
+                $scope.alertsGeneral.push({ type: 'error', msg: 'Oops, we found an error in the composition discovery process. Could you check your JSON and try again?' });
+            });
+        };
+
+
+        $scope.updateCompositionPosible = function() {
+            if(typeof defs === "undefined") $scope.compositionPosible = false;
+            for(def in $scope.defs) {
+                if($scope.defs[def].jsonDefs.length == 0) $scope.compositionPosible = false;
+            }
+            $scope.compositionPosible = true;
+        };
+
+        $scope.layout = function() {
+            if($scope.layoutRunning) {
+                $scope.layoutRunning = false;
+                $scope.layoutButton = 'Start Layout';
+                $scope.sigmaGraph.stopForceAtlas2();
+            } else {
+                $scope.layoutRunning = true;
+                $scope.layoutButton = 'Stop Layout';
+                $scope.sigmaGraph.startForceAtlas2();
+            }
+        };
+
+        $scope.rescale = function() {
+            $scope.sigmaGraph.position(0,0,1).draw();
+        };
+
     }
 ]);
