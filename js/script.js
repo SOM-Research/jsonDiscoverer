db3c2fe5ec5872192dca7e364cd10545f3c5720e
jlcanovas <me@jlcanovas.es>
2014-08-06 08:50:09 +0200

    Fixing url (again!)

--- js/script.js
+++ js/script.js
@@ -39,8 +39,8 @@ jsonDiscovererModule.config(["$routeProvider", "$httpProvider",
 jsonDiscovererModule.service('DiscovererService', ["$http",
     function($http) {
         //this.prefix = "http://apps.jlcanovas.es/jsonDiscoverer";
-        this.prefix = "http://localhost:8080/fr.inria.atlanmod.json.web";
-        //this.prefix = "http://atlanmodexp.info.emn.fr:8800/jsonDiscoverer";
+        //this.prefix = "http://localhost:8080/fr.inria.atlanmod.json.web";
+        this.prefix = "http://atlanmodexp.info.emn.fr:8800/jsonDiscoverer";
         //this.prefix = "http://localhost:8080/jsonDiscoverer";
 
         this.callService = function(call, dataToSend, success, failure) {
