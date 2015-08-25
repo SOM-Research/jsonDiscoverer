52c40856f9c326afd366aab62ca6fc543f84b985
Javier Canovas <me@jlcanovas.es>
2015-08-25 14:25:04 +0200

    First version of documentation

--- jsondiscoverer.web/WebContent/app/controllers/documentation.js
+++ jsondiscoverer.web/WebContent/app/controllers/documentation.js
@@ -1,7 +1,12 @@
-angular.module("jsonDiscoverer").controller("DocumentationCtrl", ["$scope", "$window", "$location",
-    function($scope, $window, $location) {
+angular.module("jsonDiscoverer").controller("DocumentationCtrl", ["$scope", "$window",  "$anchorScroll", "$location",
+    function($scope, $window,  $anchorScroll, $location) {
         $scope.$on('$viewContentLoaded', function(event) {
             $window.ga('send', 'pageview', {'page': '/tools/jsonDiscoverer' +$location.path()});    
         });
+		
+		$scope.scrollTo = function(id) {
+			$location.hash(id);
+			$anchorScroll();
+		}
     }
 ]);
